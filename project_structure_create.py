1b0382646c41a7942bac9c1720008f50803e3f47
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-19 17:27:37 +0200

    add modifications docs\GUIDE.md  2026-09-19 17:27:37

--- project_structure_create.py
+++ project_structure_create.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 folders = ["data","scripts","src","data/raw/","docs","data/processed/"
-           ,"data/final/","data/final/by_departement/","data/interim/"]
+           ,"data/final/","data/final/by_director/"]
 
 base = Path(".")  # dossier courant
 
@@ -19,11 +19,10 @@ print("\nContenu actuel :", os.listdir())
 
 
 # 2) files creation
-files = ["data/processed/test.txt","data/raw/test.txt",
-         "scripts/01_download_data.py","scripts/02_clean_data.py",
-         "scripts/03_split_by_departement.py","scripts/04_stats_departements.py",
-         "src/clean.py","src/download.py","docs/GUIDE.md","src/__init__.py","src/stats.py",
-         "src/split_dept.py","requirements.txt",".gitignore","README.md","data/raw/.gitkeep",
+files = ["scripts/01_download.py","scripts/02_clean.py",
+         "scripts/03_split.py","scripts/04_check.py",
+         "src/clean.py","src/download.py","src/split.py","docs/GUIDE.md","src/__init__.py",
+         "requirements.txt",".gitignore","README.md","data/raw/.gitkeep",
          "data/interim/.gitkeep","data/final/by_departement/.gitkeep"]
 
 for fi in files:
@@ -37,6 +36,12 @@ for fi in files:
                         # Cache Python\n
                         __pycache__/\n
                         *.py[cod]\n\n
+                        ```
+                        venv/
+                        __pycache__/
+                        data/raw/
+                        data/processed/
+                        ```
 
                         # Configuration locale\n
                         .env\n\n
@@ -46,16 +51,13 @@ for fi in files:
                         Thumbs.db\n\n
 
                         # Données générées\n
-                        data/raw/*\n
-                        data/interim/*\n
                         data/processed/*\n
                         data/final/*\n
 
                         # Conserver les dossiers dans Git\n
                         !data/raw/.gitkeep\n
-                        !data/interim/.gitkeep\n
                         !data/processed/.gitkeep\n
-                        !data/final/by_departement/.gitkeep\n
+                        !data/final/by_director/.gitkeep\n
 
                         # Fichiers de développement\n
                         .vscode/\n
@@ -66,10 +68,3 @@ for fi in files:
     else:
         file.write_text("# -*- coding: utf-8 -*- \n", encoding="utf-8")
         print(f"✅ Fichier créé : { file.resolve() }")
-
-
-for f in ["data/processed/test.txt", "data/raw/test.txt"]:
-    p = Path(f)
-    if p.exists():
-        p.unlink()
-        print(f"🗑️  Supprimé : {p}")
